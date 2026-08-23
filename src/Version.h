d9be1bb0d965efddd1d0a9bf696461be07bfa275
nad_22 <nad@pukepals.com>
2026-08-23 19:26:12 +0200

    chore: bump FW_VERSION to 3.5.1

--- src/Version.h
+++ src/Version.h
@@ -2,5 +2,5 @@
 
 // Firmware version of the C++ / PlatformIO rewrite (independent from the
 // legacy MicroPython release numbering).
-#define FW_VERSION "3.5.0"
+#define FW_VERSION "3.5.1"
 #define FW_MODEL "inetbox2mqtt-cpp"
